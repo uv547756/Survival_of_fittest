e5ad99ce228c6cc949e2832e5460b959f085c920
Komi <uv47756@gmail.com>
2024-12-05 01:00:43 +0530

    completed and optimizedddddddddddd

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import random
-import re
 POPULATION = 100
 GENES = '''abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890, .-;:_!"#%&/()=?@${[]}'''
 TARGET = "Utkarsh"
 
 class Individual(object):
 
-    def __init__(self):
+    def __init__(self,chromosome):
         self.chromosome = chromosome
-        self.fitness = calc_fitness()
+        self.fitness = self.calc_fitness()
 
-    @classmethod
     def calc_fitness(self):
         global TARGET
         fitness = 0
-        for gene_s, gene_t in zip(self.chromosome, chromosome)
-            if gene_s != gene_t: fitness += 1
+        for gene_s, gene_t in zip(self.chromosome, TARGET):
+            return sum(1 for gene_s, gene_t in zip(self.chromosome, TARGET) if gene_s != gene_t)
         return fitness
 
+    @classmethod
     def mutated_genes(self):
         global GENES
         gene = random.choice(GENES)
@@ -29,28 +28,51 @@ class Individual(object):
         genome_len = len(TARGET)
         return [self.mutated_genes() for _ in range(genome_len)]
     
-    def mate(self):
+    def mate(self,partner):
         child = []
-        for gparent1, gparent2 in zip(self.chromosome, par2.chromosome):
+        for gparent1, gparent2 in zip(self.chromosome, partner.chromosome):
             prob = random.random()
-            if prob < 0.4:
+            if prob < 0.45:
                 child.append(gparent1)
-            elif prob < 0.9:
+            elif prob < 0.90:
                 child.append(gparent2)
             else:
                 child.append(self.mutated_genes())
         return Individual(child)
 def main():
-    global pop_size
+    global POPULATION
     generation = 1
     found = False
-    pop = []
-
-    for _ in range(pop_size):
-        genome = Individual.genome_creation()
-        pop.append(Individual(genome))
+    # pop = []
+    population = [Individual(Individual.genome_creation()) for _ in range(POPULATION)]
 
     while not found:
-        pop = sorted(pop, key = lambda x:x.calc_fitness)
+        population = sorted(population, key = lambda x:x.fitness)
+        if population[0].fitness == 0:
+            found = True
+            break
+
+        new_gen = []
+        top_10 = int(0.10*POPULATION)
+        new_gen.extend(population[:top_10])
+        # only 10% shall survive
+
+        for _ in range(POPULATION - top_10):
+            parent1 = random.choice(population[:50])
+            parent2 = random.choice(population[:50])
+            child = parent1.mate(parent2)
+            new_gen.append(child)
+
+        population = new_gen
+        
+        print(f"Generation: {generation}\tString: {''.join(population[0].chromosome)}\tFitness: {population[0].fitness}")      
+        generation += 1
+    
+    print(f"Generation: {generation}\tString: {''.join(population[0].chromosome)}\tFitness: {population[0].fitness}")
+
+if __name__ == "__main__":
+    main()
+    
+
 
 
